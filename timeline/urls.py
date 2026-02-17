1409cd18c918c21195a3a39fb2197111337147ff
hashm109 <talha.hashmi@mail.utoronto.ca>
2026-02-17 18:08:40 -0500

    Github API calls added

--- timeline/urls.py
+++ timeline/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="Home"),
-    path("todos/", views.todos, name="Todos")
+    path("todos/", views.todos, name="Todos"),
+    path("timeline/", views.githubTimeline, name="Timeline")
 ]
--- timeline/views.py
+++ timeline/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import TodoItem
+import requests
 
 # Create your views here.
 def home(request):
@@ -8,3 +9,12 @@ def home(request):
 def todos(request):
     items = TodoItem.objects.all()
     return render(request, "todos.html", {"todos": items})
+
+def githubTimeline(request):
+
+    username = "TheLocalTalha"
+    url = f"https://api.github.com/users/{username}/events"
+    response = requests.get(url)
+    events = response.json()
+
+    return render(request, "timeline_display.html", {"events": events})
